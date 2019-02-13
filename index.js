3baf6cbb3626c7c6758a5e4e0cbdc579d95958df
Sasha Klein <sashafklein@gmail.com>
2019-02-12 22:17:31 -0800

    Add CWD

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ say(`Welcome to the Blink React boilerplate generator!\n`)
   .then(() => {
     rl.write('Ready to prep your boilerplate!');
     console.log(`Working in: ${__dirname}`);
+    console.log(`Invoked in: ${process.cwd()}`);
     // try {
     //   copy([
     //     { from: './setup/src/redux/createStore.js' },
